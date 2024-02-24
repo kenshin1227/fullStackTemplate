588be8d8ea62a83e0ba5322ec96b3e4760884913
unknown <kenshin1227@naver.com>
2024-02-24 23:38:02 +0900

    authTemplit

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,19 +1,40 @@
-const Header= (props) => {
+import { useNavigate } from "react-router-dom"
+
+const Header = (props) => {
+
+  const navigate = useNavigate()
+
+  const logoutHandler = (e) => {
+    sessionStorage.clear()
+    navigate('/account/login')
+  }
+
+  const authButtons = () => {
+    if(sessionStorage.authToken) {
+      return <div>
+        <button onClick={logoutHandler}>Log Out</button>
+      </div>
+    } else {
+      return <div className="flex flex-row justify-end items-center w-full gap-4 text-sm">
+        <li className="hover:text-red-700"><a href='/account/register'>Register</a></li>
+        <li className="hover:text-red-700"><a href='/account/login'>Login</a></li>
+      </div>
+    }
+
+  }
+
   return (
-    <nav>
-      {/* logo section */}
+    <nav className="flex flex-row flex-nowrap p-4">
+      {/* Logo Section /}
       <div>
-        <a> Logo </a>
+        <a href='/'>LOGO</a>
       </div>
 
-      {/* link lists */}
-      
-      <ul>
-        <li><a href='/account/register'> Register </a></li>
-        <li><a href='/account/login'> Login </a></li>
+      {/ link lists */}
+      <ul className="flex w-full">
+        {authButtons()}
       </ul>
     </nav>
-    
   );
 }
 export default Header;
--- frontend/src/pages/landing/auth/login.jsx
+++ frontend/src/pages/landing/auth/login.jsx
@@ -1,89 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import { useNavigate } from 'react-router-dom'
 
 const Login = (props) => {
 
-  const [ submitForm, setSubmitForm ] = useState ({
+  const [ submitForm, setSubmitForm ] = useState({
     email: '',
     password: '',
   })
-
-
-
   const [ message, setMessage ] = useState()
+
+  const navigate = useNavigate()
 
   const { email, password } = submitForm
 
   const formChangeHandler = (e) => {
-    setSubmitForm((prev)=> ({
+    setSubmitForm((prev) => ({
       ...prev,
       [e.target.name]: e.target.value
     }))
   }
 
-  const inputValidation = (email, password ) => {
-    if(!email) {
-      setMessage('please enter your email')
+  const inputValidation = (email, password) => {
+
+    if (!email) {
+      setMessage('Please enter your email')
       return false
     }
+
     if(!password) {
-      setMessage('please enter your password')
+      setMessage('Please enter password')
       return false
     }
 
     return true
   }
 
+  const clickHandler = (e) => {
+    e.preventDefault()
 
-    const clickHandler = (e) => {
-      e.preventDefault()
-
-      if(!inputValidation(email, password )) {
-        return
-      }
-
-
-
-      // todo: API call
-
-      const sendOutForm = {
-        email: email,
-        password: password
-      }
-  
-  
-      const requestToAPI = async (form) => {
-        const request = await axios.post(`${process.env.REACT_APP_BACKEND}/auth/login`, form)
-      }
-      console.log('button clicked')
-  
-      requestToAPI(sendOutForm)
-
-
-      console.log('button clicked')
+    if(!inputValidation(email, password)) {
+      return
     }
 
+    const sendOutForm = {
+      email: email,
+      password: password,
+    }
+
+    const requestToAPI = async (form) => {
+      try {
+        const request = await axios.post(`${process.env.REACT_APP_BACKEND}/auth/login`, form)
+        if(request.data) {
+          sessionStorage.setItem('authToken', request.data.token)
+          sessionStorage.setItem('userId', request.data.userId)
+          navigate('/')
+        }
+      } catch (e) {
+        setMessage(e.response.data.message)
+      }
+    } 
+    requestToAPI(sendOutForm)
+  }
+
+  useEffect(() => {
+    if(sessionStorage.token) {
+      navigate('/')
+    }
+  },[])
+
   return (
-
-    <div>
-      <p>Login </p>
-        {message && <p> {message} </p>}
-        <form onSubmit={clickHandler}>
-          <div>
-            <p>email</p>
-            <input type='email' name='email' value={email} onChange={formChangeHandler} />
-          </div>
-
-          <div>
-            <p>password</p>
-            <input type='password' name='password' value={password} onChange={formChangeHandler} />
-          </div>
-
-            <div>
-          <button type="submit"> Login </button>
-          </div>
-        </form>
+    <div className='bg-green-900 text-white flex flex-col items-center p-8 gap-4'>
+      <p className='font-bold text-xl'>Login</p>
+      {message && <p className='text-red-500'>{message}</p>}
+      <form className='flex flex-col gap-4' onSubmit={clickHandler}>
+        <div>
+          <p className='text-xs'>Email</p>
+          <input type='email' name='email' value={email} onChange={formChangeHandler} className='text-gray-700'/>
+        </div>
+        <div>
+          <p className='text-xs'>Password</p>
+          <input type='password' name='password' value={password} onChange={formChangeHandler} className='text-gray-700' />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
     </div>
   );
 }
